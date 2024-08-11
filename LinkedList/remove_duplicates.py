b513ee8eaa012c60f206d228a6a41c462ecdb318
TEKSTRUCTERS <lokesh.rajendran@structers.com>
2024-08-11 09:50:23 +0530

    added test case

--- LinkedList/remove_duplicates.py
+++ LinkedList/remove_duplicates.py
@@ -9,11 +9,29 @@ linkedList = 1 -> 1 -> 3 -> 4 -> 4 -> 4 -> 5 -> 6 -> 6 // the head node with val
 Sample Output
 1 -> 3 -> 4 -> 5 -> 6 // the head node with value 1
 """
+import unittest
 
 class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+    def addMany(self, values):
+        current = self
+        while current.next is not None:
+            current = current.next
+        for value in values:
+            current.next = LinkedList(value)
+            current = current.next
+        return self
+
+    def getNodesInArray(self):
+        nodes = []
+        current = self
+        while current is not None:
+            nodes.append(current.value)
+            current = current.next
+        return nodes
 
 
 def removeDuplicatesFromLinkedList(linkedList):
@@ -27,3 +45,12 @@ def removeDuplicatesFromLinkedList(linkedList):
         current_node = next_node
 
     return linkedList
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        test = LinkedList(1).addMany([1, 3, 4, 4, 4, 5, 6, 6])
+        expected = LinkedList(1).addMany([3, 4, 5, 6])
+        actual = removeDuplicatesFromLinkedList(test)
+        self.assertEqual(actual.getNodesInArray(), expected.getNodesInArray())
+
